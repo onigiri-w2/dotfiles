6ff0dbee0129c5cf5a2a2319f490ab120be07823
onigiri.w2 <onigiri.w2@gmail.com>
2025-11-26 02:34:35 +0900

    [mod] neovimにvitest追加

--- features/nvim/config/nvim/lua/plugins/neotest.lua
+++ features/nvim/config/nvim/lua/plugins/neotest.lua
@@ -4,11 +4,14 @@
 return {
 	{
 		"nvim-neotest/neotest",
-		dependencies = { "nvim-neotest/neotest-jest" },
+		dependencies = { "nvim-neotest/neotest-jest", "marilari88/neotest-vitest" },
 		opts = {
 			adapters = {
 				["neotest-jest"] = {
 					jestCommand = "npm test --",
+				},
+				["neotest-vitest"] = {
+					vitestCommand = "npx vitest run",
 				},
 			},
 		},
